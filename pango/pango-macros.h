6f69adc7d469402c46ecbbb8169aaf206ba7c929
dirk <dirk@git.imagemagick.org>
2013-11-07 21:08:17 +0000

    Fixed/disabled compiler warnings. FixVS2010.ps1 now also enables multiprocessor compilation.

--- pango/pango-macros.h
+++ pango/pango-macros.h
@@ -2,7 +2,15 @@
 #define __PANGO_MACROS_H__
 
 #ifndef PANGO_EXTERN
-#define PANGO_EXTERN __declspec (dllexport) extern
+#  if defined(_LIB)
+#    define PANGO_EXTERN extern
+#  else
+#    ifdef PANGO_COMPILATION
+#      define PANGO_EXTERN __declspec(dllexport)
+#    else
+#      define PANGO_EXTERN extern __declspec(dllimport)
+#    endif
+#  endif
 #endif
 
 #endif /* __PANGO_MACROS_H__ */
